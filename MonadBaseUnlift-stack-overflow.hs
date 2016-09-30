7592411c233edebee3d7d7e55b5c2caff2cd50dd
Niklas Hambüchen <mail@nh2.me>
2016-09-30 22:05:11 +0200

    Add comment

--- MonadBaseUnlift-stack-overflow.hs
+++ MonadBaseUnlift-stack-overflow.hs
@@ -1,6 +1,12 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-7.1 --install-ghc runghc --package monad-unlift-0.2.0
 
+-- Running
+--   ./MonadBaseUnlift-stack-overflow.hs
+-- crashes with
+--    *** Exception: stack overflow
+-- with GHC 8.0.1.
+
 {-# LANGUAGE FlexibleContexts #-}
 
 import Control.Monad.Trans.Unlift
